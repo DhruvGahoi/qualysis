3d592fde95ccb37287d26c84fde6a0f4d8a25e66
Mahendra Dani <danimahendra0904@gmail.com>
2024-03-17 01:10:04 +0530

    feat: added skills enum in create job

--- actions/company/createJobOpeningAction.ts
+++ actions/company/createJobOpeningAction.ts
@@ -1,17 +1,22 @@
 "use server";
 
 import { createJobOpening } from "@/services/company/createJobOpening";
-import { jobMode } from "@prisma/client";
+import { jobMode, skillsRequired } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export const createJobOpeningAction = async (formData: FormData) => {
   // TODO : Only companies should be able to create a opening
+  const skills = JSON.parse(formData.get("skills") as string);
+  let parsedSkills: skillsRequired[] = [];
+  skills.forEach((skill: skillsRequired) => {
+    parsedSkills.push(skill);
+  });
   const response = await createJobOpening({
     company_id: formData.get("company_id") as string,
     job_description: formData.get("job_description") as string,
     location: formData.get("location") as string,
     mode: formData.get("mode") as jobMode,
-    skills: [formData.get("skills") as string],
+    skills: parsedSkills,
     registration_deadline: new Date(
       formData.get("registration_deadline") as string
     ),
--- app/dashboard/company/[id]/form.tsx
+++ app/dashboard/company/[id]/form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { createJobOpeningAction } from "@/actions/company/createJobOpeningAction"
 import { Button } from "@/components/ui/button"
 import {
@@ -8,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
@@ -18,8 +20,45 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
+import { skillOptions } from "@/lib/data/skills";
+import { Badge } from "@/components/ui/badge";
+import { FormEvent, useState } from "react";
 
 export const CreateJobForm = ({ company_id }: { company_id: string }) => {
+  const [skillOpt, setskillOpt] = useState<string[]>([]);
+  const handleTags = (tag: string) => {
+    var usedSkills = [...skillOpt];
+    console.log(usedSkills);
+    if (skillOpt.includes(tag)) {
+      usedSkills.splice(usedSkills.indexOf(tag), 1);
+    } else {
+      usedSkills.push(tag);
+    }
+    setskillOpt(usedSkills);
+  };
+  const [role, setRole] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [expRequired, setExpRequired] = useState("");
+  const [mode, setMode] = useState("");
+  const [openings, setOpenings] = useState("");
+  const [registrationDeadline, setRegistrationDeadline] = useState("");
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData;
+    formData.append("skills", JSON.stringify(skillOpt));
+    formData.append("role", role);
+    formData.append("job_description", jobDescription);
+    formData.append("location", location);
+    formData.append("exp_required", expRequired);
+    formData.append("mode", mode);
+    formData.append("company_id", company_id);
+    formData.append("openings", openings);
+    formData.append("registration_deadline", registrationDeadline);
+
+    await createJobOpeningAction(formData)
+  }
   return (
     <Card className="w-[450px]">
       <CardHeader>
@@ -27,23 +66,23 @@ export const CreateJobForm = ({ company_id }: { company_id: string }) => {
         <CardDescription>Create a new job opening for your company</CardDescription>
       </CardHeader>
       <CardContent>
-        <form action={createJobOpeningAction}>
+        <form onSubmit={(e) => { handleFormSubmit(e) }}>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="role">Role</Label>
-              <Input name="role" placeholder="Frontend developer" />
+              <Input name="role" onChange={e => { setRole(e.target.value) }} placeholder="Frontend developer" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="job_description">Job Description</Label>
-              <Textarea name="job_description" placeholder="We are looking for..." />
+              <Textarea name="job_description" onChange={e => { setJobDescription(e.target.value) }} placeholder="We are looking for..." />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="location">Location</Label>
-              <Input name="location" placeholder="123, ABC Street" />
+              <Input name="location" onChange={e => { setLocation(e.target.value) }} placeholder="123, ABC Street" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="exp_required">Experience Required</Label>
-              <Select name="exp_required">
+              <Select name="exp_required" onValueChange={(value) => { setExpRequired(value) }}>
                 <SelectTrigger>
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
@@ -60,7 +99,7 @@ export const CreateJobForm = ({ company_id }: { company_id: string }) => {
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="mode">Mode</Label>
-              <Select name="mode">
+              <Select name="mode" onValueChange={(value) => { setMode(value) }}>
                 <SelectTrigger>
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
@@ -73,15 +112,53 @@ export const CreateJobForm = ({ company_id }: { company_id: string }) => {
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="openings">Number of Openings</Label>
-              <Input name="openings" placeholder="2" type="number" />
+              <Input name="openings" onChange={e => { setOpenings(e.target.value) }} placeholder="2" type="number" />
             </div>
-            <div className="flex flex-col space-y-1.5">
+            {/* <div className="flex flex-col space-y-1.5">
               <Label htmlFor="skills">Skills required</Label>
               <Textarea name="skills" placeholder="We are looking for..." />
+            </div> */}
+            <div className="flex flex-col space-y-1.5">
+              <Label className="text-lg">Product skillegory</Label>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className="w-full" variant="outline">
+                    Select Skills
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="right" className="w-56">
+                  <DropdownMenuLabel>
+                    Skills
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  {skillOptions.map((skill, id) => (
+                    <DropdownMenuCheckboxItem
+                      onClick={() => {
+                        handleTags(skill.value);
+                      }}
+                      checked={skillOpt.includes(skill.value)}
+                      key={id}
+                    >
+                      {skill.value}
+                    </DropdownMenuCheckboxItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <div className="flex flex-wrap gap-2 pt-2">
+                {skillOpt[0] != "" &&
+                  skillOpt.map((skill, id) => (
+                    <>
+                      {/* <p>{JSON.stringify(skill)}</p> */}
+                      <Badge key={id} className="w-fit">
+                        {skill}
+                      </Badge>
+                    </>
+                  ))}
+              </div>
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="registration_deadline">Application deadline</Label>
-              <Input name="registration_deadline" className="w-full" type="datetime-local" />
+              <Input name="registration_deadline" onChange={e => setRegistrationDeadline(e.target.value)} className="w-full" type="datetime-local" />
             </div>
           </div>
           <input className="hidden" name="company_id" defaultValue={company_id} />
