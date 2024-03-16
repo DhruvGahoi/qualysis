d17a08854cc89d1e0324924cbe1bd1bfe4553572
Mahendra Dani <danimahendra0904@gmail.com>
2024-03-16 12:24:15 +0530

    feat: added actions and services for onboarding

---
+++ actions/employee/createEmployeeAction.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { createEmplyee } from "@/services/employee/createEmployee";
+import { redirect } from "next/navigation";
+
+export const createEmployeeAction = async (formData: FormData) => {
+  const response = await createEmplyee({
+    user_id: formData.get("user_id") as string,
+    company_id: formData.get("company_id") as string,
+    designation: formData.get("designation") as string,
+    github_url: formData.get("github_url") as string,
+    linkedin_url: formData.get("linkedin_url") as string,
+    years_of_experience: parseInt(
+      formData.get("years_of_experience") as string
+    ),
+  });
+
+  if (response != undefined) {
+    redirect("/dashboard");
+  } else {
+    console.log("Some error from db");
+  }
+};
