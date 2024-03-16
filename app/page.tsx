9a4f03f4c47b14b102a8e54eba7ddc7a392d3e6f
Mahendra Dani <danimahendra0904@gmail.com>
2024-03-16 11:03:40 +0530

    feat: init auth and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { LoginLink, LogoutLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Home() {
   return (
     <main>
       <p>INIT</p>
-      <Button>Click Me</Button>
+      <Button>
+        <RegisterLink>Sign up</RegisterLink>
+      </Button>
+      <Button>
+        <LoginLink>Login in</LoginLink>
+      </Button>
+      <Button>
+        <LogoutLink>Logout Link</LogoutLink>
+      </Button>
     </main>
   );
 }
