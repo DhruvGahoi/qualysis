5c84bb605b25c420f3c507027afaeeebf14ceea5
Mahendra Dani <danimahendra0904@gmail.com>
2024-03-17 10:12:41 +0530

    feat: create employeee page

--- app/dashboard/company/[id]/sidebar-links.tsx
+++ app/dashboard/company/[id]/sidebar-links.tsx
@@ -12,25 +12,31 @@ export const SidebarLinks = () => {
   const homePath = `/dashboard/company/${company_id}`
   const jobsPath = `/dashboard/company/jobs/${company_id}`;
   const createJobPath = `/dashboard/company/create/${company_id}`;
+  const employeePath = `/dashboard/company/employee/${company_id}`;
+
   return (
     <main>
       <nav className="flex flex-col gap-1">
         <Link
           className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2  ${pathname == homePath ? "bg-gray-900" : "hover:bg-gray-900"} dark:text-gray-100`}
-          href={`/dashboard/company/${company_id}`}
+          href={homePath}
         >
           <HomeIcon className="h-5 w-5" />
           Home
         </Link>
-        <Link className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 ${pathname == jobsPath ? "bg-gray-900" : "hover:bg-gray-900"}`} href={`/dashboard/company/jobs/${company_id}`}>
+        <Link className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 ${pathname == jobsPath ? "bg-gray-900" : "hover:bg-gray-900"}`} href={jobsPath}>
           <ClockIcon className="h-5 w-5" />
           Jobs
         </Link>
-        <Link className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 ${pathname == createJobPath ? "bg-gray-900" : "hover:bg-gray-900"}`} href={`/dashboard/company/create/${company_id}`}>
+        <Link className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 ${pathname == createJobPath ? "bg-gray-900" : "hover:bg-gray-900"}`} href={createJobPath}>
           <ClockIcon className="h-5 w-5" />
           Create
         </Link>
-        <Link className="text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 hover:bg-gray-900" href="#">
+        <Link className={`text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 ${pathname == createJobPath ? "bg-gray-900" : "hover:bg-gray-900"}`} href={employeePath}>
+          <ClockIcon className="h-5 w-5" />
+          Employees
+        </Link>
+        <Link className="text-sm font-medium rounded-md px-3 py-2 flex items-center gap-2 hover:bg-gray-900" href={employeePath}>
           <UserIcon className="h-5 w-5" />
           Interview
         </Link>
